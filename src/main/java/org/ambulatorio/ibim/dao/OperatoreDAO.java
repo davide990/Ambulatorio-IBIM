15eb86e0455dac2d900a8d03b9a39a01e68d64c1
Domenico <domen.amato91@gmail.com>
2015-12-21 11:39:42 +0100

    -Add Method "getOperatoreBy" on OperatoreDao

--- src/main/java/org/ambulatorio/ibim/dao/OperatoreDAO.java
+++ src/main/java/org/ambulatorio/ibim/dao/OperatoreDAO.java
@@ -1,5 +1,35 @@
 package org.ambulatorio.ibim.dao;
 
-public class OperatoreDAO {
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
+import org.ambulatorio.ibim.models.Operatore;
+
+@Stateless
+public class OperatoreDAO {
+	
+	@PersistenceContext
+	private EntityManager em;
+	
+	public Operatore getOperatoreBy(String username, String psw){
+		Operatore operatore;
+		
+		TypedQuery<Operatore> q = em.createQuery("from Operatore u where u.username=? and u.password=?", Operatore.class);
+		
+		q.setParameter(1, username);
+		q.setParameter(2, psw);
+		
+		try {
+			operatore = q.getSingleResult();
+		} catch (NoResultException e) {
+			return null;
+		}
+
+		return operatore;
+		
+		
+	}
 }
