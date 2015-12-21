e9b8b0be5769af1b2a5bdb846c772612f92a5796
Mirkoa <Mirkoa@Mirko>
2015-12-21 10:59:32 +0100

    - Add Operatore and Ruolo

---
+++ src/main/java/org/ambulatorio/ibim/models/Ruolo.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.ambulatorio.ibim.models;
+
+/**
+ *
+ * @author Mirkoa
+ */
+public class Ruolo {
+    
+}
