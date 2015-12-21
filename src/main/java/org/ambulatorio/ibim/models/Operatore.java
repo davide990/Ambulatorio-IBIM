7f4a5e717cfd07c79ecec03b9726b5c4e2af5a99
Davide Guastella <davide.guastella90@gmail.com>
2015-12-21 11:05:33 +0100

    - in Operatore, fixed setPassword(...) method. - in Operatore, class implements Serializable interface.

--- src/main/java/org/ambulatorio/ibim/models/Operatore.java
+++ src/main/java/org/ambulatorio/ibim/models/Operatore.java
@@ -1,5 +1,6 @@
 package org.ambulatorio.ibim.models;
 
+import java.io.Serializable;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -9,54 +10,66 @@ import javax.persistence.ManyToOne;
 import javax.validation.constraints.Size;
 
 @Entity
-public class Operatore {
+public class Operatore implements Serializable
+{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Size(min = 5)
 
+    @Size(min = 5)
     private String username;
+
     @Size(min = 5)
     private String password;
+
     @ManyToOne
     @JoinColumn
     private Ruolo ruolo;
 
-    public Long getId() {
+    public Long getId()
+    {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(Long id)
+    {
         this.id = id;
     }
 
-    public String getUsername() {
+    public String getUsername()
+    {
         return username;
     }
 
-    public void setUsername(String username) {
+    public void setUsername(String username)
+    {
         this.username = username;
     }
 
-    public String getPassword() {
+    public String getPassword()
+    {
         return password;
     }
 
-    public void setCognome(String cognome) {
+    public void setPassword(String password)
+    {
         this.password = password;
     }
 
-    public Ruolo getRuolo() {
+    public Ruolo getRuolo()
+    {
         return ruolo;
     }
 
-    public void setRuolo(Ruolo ruolo) {
+    public void setRuolo(Ruolo ruolo)
+    {
         this.ruolo = ruolo;
     }
 
     @Override
-    public int hashCode() {
+    public int hashCode()
+    {
         final int prime = 31;
         int result = 1;
         result = prime * result + ((id == null) ? 0 : id.hashCode());
@@ -64,22 +77,29 @@ public class Operatore {
     }
 
     @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
+    public boolean equals(Object obj)
+    {
+        if (this == obj)
+        {
             return true;
         }
-        if (obj == null) {
+        if (obj == null)
+        {
             return false;
         }
-        if (getClass() != obj.getClass()) {
+        if (getClass() != obj.getClass())
+        {
             return false;
         }
         Operatore other = (Operatore) obj;
-        if (id == null) {
-            if (other.id != null) {
+        if (id == null)
+        {
+            if (other.id != null)
+            {
                 return false;
             }
-        } else if (!id.equals(other.id)) {
+        } else if (!id.equals(other.id))
+        {
             return false;
         }
         return true;
