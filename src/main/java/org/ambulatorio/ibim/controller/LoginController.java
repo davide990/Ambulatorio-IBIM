d307a0708c9f10960497cd783d14094c09cc4153
Davide Guastella <davide.guastella90@gmail.com>
2015-12-21 11:10:00 +0100

    - Added LoginController class. - Fixed Ruolo class.

---
+++ src/main/java/org/ambulatorio/ibim/controller/LoginController.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.ambulatorio.ibim.controller;
+
+/**
+ *
+ * @author Davide Guastella <davide.guastella90@gmail.com>
+ */
+public class LoginController
+{
+    
+}
